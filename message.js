2eb2a05c1414a82cd2ed8ff5e1b4dbf1db4590b1
maryam <maryam.pourganji92@gmail.com>
2022-04-30 16:49:46 +0430

    variables

--- message.js
+++ message.js
@@ -1,2 +1,15 @@
-let message= prompt("what is your name??");
-alert(message);
+// let message= prompt("what is your name??");
+// alert(message);
+
+alert("Hello! welcome \n fill out the form please");
+
+let first = prompt("What is your name? ");
+let gender = prompt("what is your gender? ");
+let age = prompt("How old are you? ");
+alert( first + ' '+ gender + ' ' + age);
+
+
+let dayofbirth = prompt("what is day of your birthday?");
+let yearofbirth = prompt("In what year were you born? ");
+alert("dayofbirth : "+ dayofbirth + " yearofbirth : " + yearofbirth);
+alert("dayofbirth : "+ yearofbirth + " yearofbirth : "+ dayofbirth );
